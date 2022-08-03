a2dcb3c1edcee7382183c0c3fad2bd9f1a1361dc
thi-costa <thiago14abr@gmail.com>
2022-08-02 22:05:21 -0300

    POST method implemented

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ server.get("/clients/:index", (req, res) => {
     return res.json(req.user);
 });
 
+server.post("/clients", checkClientExists, (req, res) => {
+    const { email, name, username, password } = req.body; // Name in the request
+
+    clients.push(name);
+
+    return res.json(clients); // Returns clients variable information
+});
+
 
 server.listen(3000); // server executed on port 3000 from localhost:3000
 

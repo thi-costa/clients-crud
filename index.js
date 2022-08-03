c4403bf7e896939dea7de5356a5fcda22db2c751
thi-costa <thiago14abr@gmail.com>
2022-08-02 22:04:24 -0300

    Empty Clients array created

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const server = express(); // variable calls express function
 
 server.use(express.json()); // Makes the express understand JSON
 
+const clients = [];
+
 
 server.listen(3000); // server executed on port 3000 from localhost:3000
 

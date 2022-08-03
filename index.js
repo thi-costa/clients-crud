6000026277c51707b5bc660b030d0dcef71d0e43
Thiago Costa <41833533+thi-costa@users.noreply.github.com>
2022-08-02 22:04:47 -0300

    GET routes implemented

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ server.use(express.json()); // Makes the express understand JSON
 
 const clients = [];
 
+server.get("/clients", (req, res) => {
+    return res.json(clients);
+}); // Route to list clients
+
+server.get("/clients/:index", (req, res) => {
+    return res.json(req.user);
+});
+
 
 server.listen(3000); // server executed on port 3000 from localhost:3000
 

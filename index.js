c8999887eae1e7ee80416b06d6f7f246ee774be9
Thiago Costa <41833533+thi-costa@users.noreply.github.com>
2022-08-02 22:03:44 -0300

    Server Created

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express"); // express imported
+
+const server = express(); // variable calls express function
+
+server.use(express.json()); // Makes the express understand JSON
+
+
+server.listen(3000); // server executed on port 3000 from localhost:3000
+
